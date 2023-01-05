87be1b649991a0110a2ace5c7468a913fd9fd9c0
InfernalPyro <lorenzopassaretti@libero.it>
2023-01-05 16:23:55 +0100

    Restored Permissions

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,12 +1,13 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import NewFountainView from '../views/NewFountainView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
 		{path: '/', component: HomeView},
-		{path: '/new', component: NewFountainView},
+		{path: '/link1', component: HomeView},
+		{path: '/link2', component: HomeView},
+		{path: '/some/:id/link', component: HomeView},
 	]
 })
 
