cc694a3870cf761b61706970f649a5b7caa30d4f
InfernalPyro <lorenzopassaretti@libero.it>
2023-01-15 21:33:38 +0100

    Response body must be closed

--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -39,8 +39,10 @@ func main() {
 		_, _ = fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	} else if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNoContent {
+		_ = res.Body.Close()
 		_, _ = fmt.Fprintln(os.Stderr, "Healthcheck request not OK: ", res.Status)
 		os.Exit(1)
 	}
+	_ = res.Body.Close()
 	os.Exit(0)
 }
